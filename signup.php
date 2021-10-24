86844002abc9d5b1f4fc1bdb4c714849468f8c43
alarebo1 <alarebo1@student.gsu.edu>
2021-10-24 00:31:57 -0400

    style change

--- signup.php
+++ signup.php
@@ -9,9 +9,9 @@ foreach ($userlist as $user)
 {
     $users = explode('|', $user);
 }
-if (in_array($userN, $users, true)) {
+if (in_array($userN, $users)) {
     
-    echo "User exisits";
+    $message = "user already exists";
 } 
 else {
 $score = '00';
@@ -25,25 +25,48 @@ header('Location: login.php');
 }
 ?>
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="./static/style.css" />
-    <title></title>
-  </head> 
-  <body>
-    <h1 align="center">Create Acount</h1><br>
-
-    <form action="" method="post">
-        <div class="container">
-            <label for="newuname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required><br>
-        
-            <label for="newpsw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required><br>      
-            <button type="submit" name="create" value="create">Create Account</button>
-            <a href="login.php">Already have Account</a>   
+<html lang="en">  
+    <head>
+        <title>Who wants to be Millonair</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="static/main.css">
+    </head>
+    <body>
+        <div class="formframe">
+            <div class="formlogin">
+                <div class="wrap-login100">
+                    <form class="loginvalidation" action="" method="post" >
+                        <span class="login100-form-title">
+                            New Account
+                        </span><br><br>
+                        <div class="wrap-input100" >
+                            <span class="focus-input100" ><label for="newuname" class="focus-input100"><b>Username</b></label></span> <br>
+                            <input class="input100"type="text" name="uname" required>
+                        </div>
+                        <div class="wrap-input100 ">
+                            <span class="focus-input100" ><label for="newpass" class="focus-input100"><b>Password</b></label></span> <br>
+                            <input class="input100" type="password" name="psw" required>
+                        </div>
+                        <div class="formlogin-form-btn">
+                            <div class="wrap-login100-form-btn">
+                                <div class="login100-form-bgbtn"></div>
+                                    <button class="login100-form-btn" type="submit" name="create" value="create">
+                                        Sign up
+                                    </button>
+                            </div>
+                        </div>
+                        <div class="text-center p-t-115">
+                            <span class="txt1">
+                                Already have an account?
+                            </span>
+                            <a class="txt2" href="login.php">
+                                Login
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </form>
-  </body>
+    </body>
 </html>
